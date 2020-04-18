ecc8b5404e4a7845a4fc86e9ae229455569a6b4a
Alex Berryman <alexbman@gmail.com>
2020-04-18 18:42:32 -0500

    run migrations from the bot to test cold heroku deploy

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	_ "github.com/lib/pq"
+	migrate "github.com/rubenv/sql-migrate"
 	"log"
 	"os"
 	"os/signal"
@@ -41,6 +42,27 @@ func init() {
 	}
 
 	db = dbConnection
+
+	migrations := &migrate.FileMigrationSource{
+		Dir: "internal/turnips/schema",
+	}
+
+	DatabaseUrl := os.Getenv("DATABASE_URL")
+	if DatabaseUrl == "" {
+		log.Fatal("databaseUrl must be set")
+	}
+
+	db, err := sql.Open("postgres", DatabaseUrl)
+	if err != nil {
+		log.Fatal("cannot connect to database:", err)
+	}
+
+	n, err := migrate.Exec(db, "postgres", migrations, migrate.Up)
+	if err != nil {
+		log.Fatal("migrations failed to up", err)
+	}
+
+	log.Printf("Applied %d migrations!\n", n)
 }
 
 func main() {

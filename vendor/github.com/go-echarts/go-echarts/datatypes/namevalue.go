4efe970b6867010ac76b2b272815f74200e1de45
Alex Berryman <alexbman@gmail.com>
2020-04-19 18:04:20 -0500

    re vendor

--- vendor/github.com/go-echarts/go-echarts/datatypes/namevalue.go
+++
@@ -1,7 +0,0 @@
-package datatypes
-
-// NameValueItem represents a data type with a name and a value.
-type NameValueItem struct {
-	Name  string      `json:"name"`
-	Value interface{} `json:"value"`
-}

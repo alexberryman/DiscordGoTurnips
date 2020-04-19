0ef490f13e22a7e97cbd42e1cfae03e02613733f
Alex Berryman <alexbman@gmail.com>
2020-04-19 17:30:35 -0500

    handle a real favicon

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -189,8 +189,15 @@ func renderSharpLine(w http.ResponseWriter, periods []string, serverID string, p
 	_ = line.Render(w, f)
 }
 
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("%s\n", r.RequestURI)
+	w.Header().Set("Content-Type", "image/x-icon")
+	w.Header().Set("Cache-Control", "public, max-age=7776000")
+	fmt.Fprintln(w, "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=\n")
+}
+
 func main() {
-	http.HandleFunc("/favicon.ico", nil)
+	http.HandleFunc("/favicon.ico", faviconHandler)
 	http.HandleFunc("/", handler)
 	log.Println("Run server at " + os.Getenv("CUSTOM_DOMAIN") + ":" + os.Getenv("PORT"))
 	err := http.ListenAndServe(":"+port, nil)
